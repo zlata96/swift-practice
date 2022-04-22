403227fa3fa8b18af6dad8c0eff8032df599e39c
Zlata Guseva <gusevazlata96@gmail.com>
2022-04-22 18:24:00 +0300

    Homework 1

---
+++ Skutarenko.playground/Pages/Homework-1.xcplaygroundpage/Contents.swift
@@ -0,0 +1,9 @@
+let name = "Zlata"
+let surname = "Guseva aka Bystritskaya"
+let weight = 62
+let growth = 168
+let age = 25
+
+print("Name of student \(name) \(surname)")
+print("Her age is \(age)")
+print("Her weight is \(weight) and her growth is \(growth)")
