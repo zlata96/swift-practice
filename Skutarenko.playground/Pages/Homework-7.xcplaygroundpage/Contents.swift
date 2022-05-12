974cdc4c6e814abb10de5e59150e6682053e1e1c
Zlata Guseva <gusevazlata96@gmail.com>
2022-05-12 22:15:31 +0300

    Homework 7

---
+++ Skutarenko.playground/Pages/Homework-7.xcplaygroundpage/Contents.swift
@@ -0,0 +1,94 @@
+// TASK 1:
+
+let daysOfMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+print(daysOfMonth)
+let nameOfMonth = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+for i in 0...daysOfMonth.count - 1 {
+    print ("there are \(daysOfMonth[i]) days in \(nameOfMonth[i])")
+}
+
+typealias MyTuple = (days: Int, month: String)
+var nameAndDaysArray: [MyTuple] = []
+
+for i in 0...daysOfMonth.count - 1 {
+    nameAndDaysArray.append((daysOfMonth[i], nameOfMonth[i]))
+}
+
+var index = nameAndDaysArray.count - 1
+
+for _ in 0...nameAndDaysArray.count - 1 {
+    print(nameAndDaysArray[index])
+    index -= 1
+}
+
+let monthOfBirthday = 10
+let dayOfBirthday = 22
+var sumOfDaysBeforeBTHD = 0
+for i in 0...monthOfBirthday - 2 {
+    sumOfDaysBeforeBTHD += daysOfMonth[i]
+}
+sumOfDaysBeforeBTHD += dayOfBirthday
+print("There are \(sumOfDaysBeforeBTHD) days will be passed since the beggining of the year until my birthday")
+
+// TASK 2:
+
+let optionalArray: [Int?] = [5, nil, 4583, 67, nil]
+var sumOfElements = 0
+
+// a) Optional binding
+
+for i in 0...optionalArray.count - 1 {
+    if let a = optionalArray[i] {
+    sumOfElements += a
+    }
+}
+print(sumOfElements)
+
+// b) Force unwraped
+
+sumOfElements = 0
+
+for i in 0...optionalArray.count - 1 {
+    if optionalArray[i] != nil {
+    sumOfElements += optionalArray[i]!
+    }
+}
+print(sumOfElements)
+
+// c) ?? (Nil-Coalescing Operator)
+
+sumOfElements = 0
+
+for i in 0...optionalArray.count - 1 {
+    sumOfElements += optionalArray[i] ?? 0
+    }
+print(sumOfElements)
+
+// TASK 3: Create reversed array from string
+
+var englishAlphabet = "abcdefghijklmnopqrstuvwxyz"
+var reversedEnglishAlphabetArray: [String] = []
+
+// First way:
+// string - > array
+
+var englishAlphabetArray = Array(englishAlphabet)
+
+for i in 0...englishAlphabetArray.count - 1 {
+    reversedEnglishAlphabetArray.append(String(englishAlphabetArray[englishAlphabet.count - 1 - i]))
+}
+
+print(reversedEnglishAlphabetArray)
+
+// Second way:
+// reversed()
+
+reversedEnglishAlphabetArray = []
+
+for character in englishAlphabet {
+    reversedEnglishAlphabetArray.append(String(character))
+}
+reversedEnglishAlphabetArray = reversedEnglishAlphabetArray.reversed()
+
+print(reversedEnglishAlphabetArray)
