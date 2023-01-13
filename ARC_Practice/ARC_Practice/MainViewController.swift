f6e9d3f0462ff0d45a7dcd4cc1ad83d2b147aae5
Zlata Guseva <gusevazlata96@gmail.com>
2023-01-13 20:24:12 +0400

    ARC Practice

---
+++ ARC_Practice/ARC_Practice/MainViewController.swift
@@ -0,0 +1,115 @@
+import UIKit
+
+class MainViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    runScenario()
+  }
+
+  func runScenario() {
+    let user = User(name: "John")
+    let iPhone = Phone(model: "iPhone Xs")
+    user.add(phone: iPhone)
+    iPhone.owner = user
+
+    let subscription = CarrierSubscription(
+      name: "TelBel",
+      countryCode: "0032",
+      number: "31415926",
+      user: user)
+    iPhone.provision(carrierSubscription: subscription)
+
+    print(subscription.completePhoneNumber())
+
+    let greetingMaker: () -> String
+
+    do {
+      let mermaid = WWDCGreeting(who: "caffeinated mermaid")
+      greetingMaker = mermaid.greetingMaker
+    }
+
+    print(greetingMaker())
+  }
+}
+
+class User {
+  let name: String
+  var subscriptions: [CarrierSubscription] = []
+  private(set) var phones: [Phone] = []
+
+  init(name: String) {
+    self.name = name
+    print("User \(name) was initialized")
+  }
+
+  deinit {
+    print("Deallocating user named: \(name)")
+  }
+
+  func add(phone: Phone) {
+    phones.append(phone)
+    phone.owner = self
+  }
+}
+
+class Phone {
+  let model: String
+  weak var owner: User?
+  var carrierSubscription: CarrierSubscription?
+
+  init(model: String) {
+    self.model = model
+    print("Phone \(model) was initialized")
+  }
+
+  deinit {
+    print("Deallocating phone named: \(model)")
+  }
+
+  func provision(carrierSubscription: CarrierSubscription) {
+    self.carrierSubscription = carrierSubscription
+  }
+
+  func decommission() {
+    carrierSubscription = nil
+  }
+}
+
+class CarrierSubscription {
+  let name: String
+  let countryCode: String
+  let number: String
+  unowned let user: User
+  lazy var completePhoneNumber: () -> String = { [unowned self] in
+    return self.countryCode + " " + self.number
+  }
+
+  init(name: String, countryCode: String, number: String, user: User) {
+    self.name = name
+    self.countryCode = countryCode
+    self.number = number
+    self.user = user
+    user.subscriptions.append(self)
+
+    print("CarrierSubscription \(name) is initialized")
+  }
+
+  deinit {
+    print("Deallocating CarrierSubscription named: \(name)")
+  }
+}
+
+class WWDCGreeting {
+  let who: String
+
+  init(who: String) {
+    self.who = who
+  }
+
+  lazy var greetingMaker: () -> String = { [weak self] in
+    guard let self = self else {
+      return "No greeting available."
+    }
+    return "Hello \(self.who)."
+  }
+}
