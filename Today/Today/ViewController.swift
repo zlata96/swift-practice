54393c0cb3bf634908e759d84910ebfe3c52a379
Zlata Guseva <gusevazlata96@gmail.com>
2023-01-17 21:05:50 +0400

    Initial commit

---
+++ Today/Today/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Today
+//
+//  Created by Злата Гусева on 16.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
