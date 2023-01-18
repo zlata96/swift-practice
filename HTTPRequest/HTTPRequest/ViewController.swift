0018d638bf5f6f25f163fb4e54c2dc501189f857
Zlata Guseva <gusevazlata96@gmail.com>
2023-01-18 23:15:48 +0400

    Refactoring

--- HTTPRequest/HTTPRequest/ViewController.swift
+++ HTTPRequest/HTTPRequest/ViewController.swift
@@ -7,41 +7,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-    let sessionConfiguration = URLSessionConfiguration.default
-    let session = URLSession.shared
-    let decoder = JSONDecoder()
+    let networkManager = NetworkManager()
     var dataSource = [Post]()
 
     @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-obtainPosts()
         tableView.dataSource = self
-    }
-
-    func obtainPosts() {
-        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else {return }
-        session.dataTask(with: url) {[weak self] data, response, error in
-            guard let strongSelf = self else {return }
-            if error == nil, let parsData = data {
-                guard let posts = try? strongSelf.decoder.decode([Post].self, from: parsData) else {return }
-                strongSelf.dataSource = posts
-
+        networkManager.obtainPosts { [weak self] (result) in
+            switch result {
+            case .success(let posts) :
+                self?.dataSource = posts
                 DispatchQueue.main.async {
-                    strongSelf.tableView.reloadData()
+                    self?.tableView.reloadData()
                 }
-            } else {
-                print("Error is \(error?.localizedDescription)")
+            case .failure(let error) :
+                print(error)
             }
-        }.resume()
+        }
     }
 }
 
 extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            dataSource.count
-        }
+        dataSource.count
+    }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "identifier", for: indexPath)
